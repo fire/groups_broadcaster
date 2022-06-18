53d4f411b31e43140a93e9368b41cb7ed071b328
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-06-18 12:57:25 -0700

    Add initial files.

---
+++ lib/groups_broadcaster.ex
@@ -0,0 +1,18 @@
+defmodule GroupsBroadcaster do
+  @moduledoc """
+  Documentation for `GroupsBroadcaster`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GroupsBroadcaster.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/groups_broadcaster_test.exs
@@ -0,0 +1,8 @@
+defmodule GroupsBroadcasterTest do
+  use ExUnit.Case
+  doctest GroupsBroadcaster
+
+  test "greets the world" do
+    assert GroupsBroadcaster.hello() == :world
+  end
+end
