de8f7fa02fb6f60d152a5695b8607b030f209353
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-06-18 13:59:05 -0700

    Switch to a task to serve.

--- lib/broadcaster/server.ex
+++ lib/broadcaster/server.ex
@@ -10,7 +10,7 @@ defmodule Broadcaster.Server do
 
   defp loop_acceptor(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    serve(client)
+    Task.start_link(fn -> serve(client) end)
     loop_acceptor(socket)
   end
 
